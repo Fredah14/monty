a45a6367fefdcccb70f49bd51b04de6ec616d014
Fredah14 <fredah.jr@hotmail.com>
2023-08-25 12:15:43 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -51,7 +51,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void _push(stack_t **stack, unsigned int line_number);
-void _pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **head, unsigned int oount);
 void _pop(stack_t **head, unsigned int oount);
 void _swap(stack_t **head, unsigned int oount);
