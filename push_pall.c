9d093f4162fa55f7371bf08e268c5356a9ddc373
Fredah14 <fredah.jr@hotmail.com>
2023-08-25 11:29:50 -0700

    0. push, pall

--- push_pall.c
+++ push_pall.c
@@ -5,6 +5,7 @@
 /**
  * push - adds a node to the stack
  * @head: the stack head
+ * @count: the line number
  * Return: no return
  */
 
@@ -12,8 +13,52 @@ stack_t *stack = NULL;
 
 void push(stack_t **stack, unsigned int line_number)
 {
+int k, l;
+  
 if (!instruction_t.args)
 {
+dprintf(2, "L%u: ", count);
+dprintf(2, "usage: push integer\n");
+free_instruction_t();
+exit(EXIT_FAILURE);
+}
+for (l=0; instruction_t.argsl] != '\0'; l++)
+{
+if (!isdigit(instruction_t.argsl] && instruction_t.args[l] != '-)
+{
+dprintf(2, "L%u: ", count);
+dprintf(2, "usage: push integer\n");
+free_instruction_t();
+exit(EXIT_FAILURE);
+}
+}
+n = atoi(instruction_t.args);
+if (instruction_t.lifo == 1)
+add_dnodeint(head, n);
+else
+add_dnodeint(head, n);
+}
+
+/**
+ * pall - prints values on the stack
+ * @head: the stack head
+ * @count: the line number
+ * Return: no return
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+stack_t *current;
+(void) count;
+*count = *head;
+ 
+while (current)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+}
+
 fprintf(stderr, "L%u: usage: push integer\n", line_number);
 exit(EXIT_FAILURE);
 }
@@ -37,51 +82,5 @@ if (*stack)
 *stack = new_node;
 }
 
-void pall(stack_t **stack, unsigned int line_number)
-{
-stack_t *current = *stack;
-
-while (current)
-{
-printf("%d\n", current->n);
-current = current->next;
-}
-}
-/**
- * main - code interpreter
- * @argc: the number of arguments
- * @argv: the location
- *
- * Return: 0 on success
- */
-
-int main(int argc, char *argv[])
-{
-char *content;
-FILE *file;
-size_t size = 0;
-ssize_t read_line = 1;
-stack_t *stack = NULL;
-unsigned int count = 0;
-  
-if (strcmp(instruction_t.opcode, "push") == 0)
-{
-push(&stack, line_number);
-}
-else if (strcmp(instruction_t.opcode, "pall") == 0)
-{
-pall(&stack, line_number);
-}
-else
-{
-fprintf(stderr, "L%u: unknown instruction %s\n", line_number, instruction_t.opcode);
-exit(EXIT_FAILURE);
-}
-
-n = atoi(instruction_t.args);
-if (instruction_t.lifi == 0)
-addnode(head, n);
-else
-addqueue(head,n= n);
 return 0;
 }
